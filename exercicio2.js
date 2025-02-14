91cb1bddac09965f9eed5029c4403e3e65e6b25d
SunaUezuri <wesley.jane.santos@gmail.com>
2025-02-13 22:03:38 -0300

    Utilizando estruturas condicionais para dizer se uma variável tem valor nulo ou undefined.

---
+++ exercicio2.js
@@ -0,0 +1,14 @@
+let valorNulo = null;
+let valorIndefinido;
+
+if (valorNulo === null) {
+    console.log("O valor é nulo");
+} else {
+    console.log("O valor é indefinido");
+}
+
+if (valorIndefinido === undefined) {
+    console.log("O valor é indefinido");
+} else {
+    console.log("O valor é nulo");
+}
