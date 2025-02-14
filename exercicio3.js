c9c0906955e8f25fc46113a56de578e889852f27
SunaUezuri <wesley.jane.santos@gmail.com>
2025-02-13 22:08:20 -0300

    Criando condicionais para dizer se um valor declarado é positivo, negativo ou zero.

---
+++ exercicio3.js
@@ -0,0 +1,9 @@
+let valor = 0
+
+if (valor > 0){
+    console.log("O valor é positivo");
+} else if (valor < 0) {
+    console.log("O valor é negativo");
+} else {
+    console.log("O valor é zero");
+}
