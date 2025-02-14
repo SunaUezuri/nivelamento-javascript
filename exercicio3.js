569caeb890650b58aa5ba057486c13a3fd6aace8
SunaUezuri <wesley.jane.santos@gmail.com>
2025-02-13 22:14:34 -0300

    Utilizando um lçao while para realizar a soma de 1 a 5 utilizando as variaveis "numero" e "soma".

--- exercicio3.js
+++ exercicio3.js
@@ -11,3 +11,15 @@ if (valor > 0){
 for (let i = 0; i < 11; i++) {
     console.log(i);
 }
+
+//Criando variáveis para realizar o loop de soma
+
+let numero = 1 //Começa em 1
+let soma = 0; //Variável para inicializar a soma
+
+while(numero <= 5) {
+    soma += numero; //Soma o número ao acumulador
+    numero++; //Incrementa o número
+}
+
+console.log("Soma dos números de 1 a 5:", soma);
